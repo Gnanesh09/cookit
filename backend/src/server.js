143a626977b50601253c8eebdd90ee551d9703d5
Gnanesh G <gnanesh9206@gmail.com>
2025-12-01 19:45:11 +0530

    server.js

---
+++ backend/src/server.js
@@ -0,0 +1,7 @@
+import express from "express"
+import  "dotenv/config"
+const app = express()
+const port = process.env.PORT || 8001
+app.listen(port,()=>{
+    console.log("server runnig at port: ",port)
+})
