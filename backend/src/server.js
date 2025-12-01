d7bacac34bfa114b4bf73f12d1c3b8929d37c3f3
Gnanesh G <gnanesh9206@gmail.com>
2025-12-01 23:10:45 +0530

    database schema

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,11 @@ import express from "express"
 import  "dotenv/config"
 const app = express()
 const port = process.env.PORT || 8001
+
+app.get("/api/health", (req,res)=>{
+    res.status(200).json({success:true})
+})
+
 app.listen(port,()=>{
     console.log("server runnig at port: ",port)
 })
